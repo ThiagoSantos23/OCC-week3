948d8be72e3ff2789a007bf980e0fb0715d1493b
ThiagoSantos23 <73845416+ThiagoSantos23@users.noreply.github.com>
2021-10-28 12:16:57 +0100

    V2
    This is a test

--- src/occ/week3/NewClass.java
+++ src/occ/week3/NewClass.java
@@ -9,15 +9,10 @@ package occ.week3;
  *
  * @author thsan
  */
-public class OCCWeek3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("This is my first repository");
+public class NewClass {
+    
+    public NewClass(){
+        System.out.println("New Class");
     }
     
 }
