fd35d8c63b3ce67c2bd2d94f731d4cd3ac2290ba
ThiagoSantos23 <73845416+ThiagoSantos23@users.noreply.github.com>
2021-10-28 12:34:07 +0100

    v6
    testing

--- src/occ/week3/OCCWeek3.java
+++ src/occ/week3/OCCWeek3.java
@@ -17,7 +17,7 @@ public class OCCWeek3 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("This is my first repository");
+        //System.out.println("This is my first repository");
         
         System.out.println("This is my first repository");
         System.out.println("This is my first repository");
